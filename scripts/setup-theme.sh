03b0738f97f64f73ac5fe890d0f166456e35eb35
OnyxJeff <j.may2702@gmail.com>
2026-06-13 01:12:55 -0400

    added snapping

--- scripts/setup-theme.sh
+++ scripts/setup-theme.sh
@@ -57,6 +57,43 @@ if command -v gsettings >/dev/null 2>&1; then
 fi
 
 # --------------------------------------------------
+# Windows 10/11-like snapping (labwc)
+# --------------------------------------------------
+
+echo "[*] Writing labwc config for snapping and theming..."
+
+cat > "$REAL_HOME/.config/labwc/rc.xml" <<EOF
+<?xml version="1.0"?>
+<openbox_config xmlns="http://openbox.org/3.4/rc">
+  <theme>
+    <font place="ActiveWindow">
+      <name>Nunito Sans</name>
+      <size>12</size>
+      <weight>Light</weight>
+      <slant>Normal</slant>
+    </font>
+    <font place="InactiveWindow">
+      <name>Nunito Sans</name>
+      <size>12</size>
+      <weight>Light</weight>
+      <slant>Normal</slant>
+    </font>
+    <name>PiXonyx</name>
+  </theme>
+  <snapping>
+    <range>
+      <inner>20</inner>
+      <outer>20</outer>
+    </range>
+    <topMaximize>yes</topMaximize>
+    <overlay>
+      <enabled>yes</enabled>
+    </overlay>
+  </snapping>
+</openbox_config>
+EOF
+
+# --------------------------------------------------
 # OWNERSHIP FIX
 # --------------------------------------------------
 
